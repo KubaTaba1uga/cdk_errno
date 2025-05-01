ae8712e475725eac280b11cc2919a3f14bc15ece
KubaTaba1uga <luna7337anul@proton.me>
2025-05-01 09:20:25 +0200

    optimize static struct

--- docs/static_vs_dynamic_error/static.h
+++ docs/static_vs_dynamic_error/static.h
@@ -1,28 +1,31 @@
 #ifndef C_MINILIB_STATIC_ERROR_H
 #define C_MINILIB_STATIC_ERROR_H
 
+#include <inttypes.h>
 #include <stdio.h>
 
 #define CME_STACK_MAX 16
-#define CME_STR_MAX 255
-
-struct cme_StaticError {
-  int code;
-  char msg[CME_STR_MAX];
-  // These needs to be string literals
-  const char *source_file;
-  const char *source_func;
-  int source_line;
+#define CME_STR_MAX 256
+// Because we want to hit cpu hot cache when operating on error
+// error should be less than 32kib, as this is most common
+// 64bit system L1 cache these days.
+struct __attribute__((aligned(8))) cme_StaticError {
+  uint32_t code;           // 32/8 = 4 bytes
+  char msg[CME_STR_MAX];   // 1*255 = 255 bytes (but aligned to 256)
+  const char *source_file; // 64/8 = 8 bytes
+  const char *source_func; // 64/8 = 8 bytes
+  uint32_t source_line;    // 32/8 = 4 bytes
 #ifdef CME_ENABLE_BACKTRACE
-  int stack_length;
-  // We store ptrs to funcs only
-  void *stack_symbols[CME_STACK_MAX];
+  uint32_t stack_length;              // 32/8 = 4 bytes
+  void *stack_symbols[CME_STACK_MAX]; // (64/8) * 16 = 128 bytes
 #else
   // This is not filled, we just want to keep API happy.
-  int stack_length;
-  void **stack_symbols;
+  uint32_t stack_length; // 32/8 = 4 bytes
+  void **stack_symbols;  // 64/8 = 8 bytes
 #endif
 };
+// If CME_ENABLED_BACKTRACE defined we have 4+255+8+8+4+4+128 = 412 bytes.
+// If not we have 4+256+8+8+4+4+8 = 292 bytes
 
 typedef struct cme_StaticError *cme_static_error_t;
 
